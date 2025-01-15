b9403897f0180807778217eebc0bb8f5df116b71
Eduard Galikhanov <godlik04@mail.ru>
2025-01-15 23:06:42 +0300

    feat: обработка лет в профиле

--- src/components/atoms/ServiceExperienceBadge.vue
+++ src/components/atoms/ServiceExperienceBadge.vue
@@ -2,11 +2,30 @@
 import type { Props } from '@/types/components/atoms/ServiceExperienceBadgeInterface'
 
 defineProps<Props>()
+
+function formatExperienceYears(years: number) {
+  const lastDigit = years % 10
+  const lastTwoDigits = years % 100
+
+  let suffix = ''
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 19) {
+    suffix = 'лет'
+  } else if (lastDigit === 1) {
+    suffix = 'год'
+  } else if (lastDigit >= 2 && lastDigit <= 4) {
+    suffix = 'года'
+  } else {
+    suffix = 'лет'
+  }
+
+  return `${years} ${suffix}`
+}
 </script>
 
 <template>
   <div class="d-flex flex-nowrap rounded-pill master-skill">
     <span>{{ activity.activityTemplateName }}</span>
-    <span class="rounded-pill">{{ activity.experience }}</span>
+    <span class="rounded-pill">{{ formatExperienceYears(activity.experience) }}</span>
   </div>
 </template>
